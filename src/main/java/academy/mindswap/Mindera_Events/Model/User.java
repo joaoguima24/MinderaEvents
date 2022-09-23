ccd4883acaaf015f1fa0cf04e04581555ff46428
fabioiketani <fabio.iketani@gmail.com>
2022-09-23 12:39:32 +0100

    Users model

--- src/main/java/academy/mindswap/Mindera_Events/Model/User.java
+++ src/main/java/academy/mindswap/Mindera_Events/Model/User.java
@@ -1,4 +1,22 @@
 package academy.mindswap.Mindera_Events.Model;
 
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
 public class User {
+    @Id
+    private String id;
+    private String email;
+    private String events;
+    private String password;
+    private String officeRole;
+    private String dateOfBirth;
+
+    public User(){
+
+    }
 }
