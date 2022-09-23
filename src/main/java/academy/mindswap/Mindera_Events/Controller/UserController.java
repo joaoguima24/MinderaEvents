a4d86bb8d2316bec6e7e326726ea5e799acf7e59
fabioiketani <fabio.iketani@gmail.com>
2022-09-23 15:37:53 +0100

    controllers gets

--- src/main/java/academy/mindswap/Mindera_Events/Controller/UserController.java
+++ src/main/java/academy/mindswap/Mindera_Events/Controller/UserController.java
@@ -1,12 +1,17 @@
 package academy.mindswap.Mindera_Events.Controller;
 
 import academy.mindswap.Mindera_Events.Model.Event;
+import academy.mindswap.Mindera_Events.Model.User;
 import academy.mindswap.Mindera_Events.Service.EventService;
 import academy.mindswap.Mindera_Events.Service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController("/user")
 public class UserController {
@@ -23,5 +28,14 @@ public class UserController {
     public Event createEvent(@RequestBody Event event){
         return eventService.createEvent(event);
     }
-
+    @GetMapping
+    public ResponseEntity<List<User>> getUserList() {
+        List<User> userList = userService.getUserList();
+        return new ResponseEntity<>(userList, HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<List<Event>> getEventList() {
+        List<Event> eventList = eventService.getEventList();
+        return new ResponseEntity<>(eventList, HttpStatus.OK);
+    }
 }
