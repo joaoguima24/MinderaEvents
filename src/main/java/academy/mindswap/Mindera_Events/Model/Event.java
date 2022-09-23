5a2f15183f13f50e1fba305ac98cb6720e201ab3
David Mendes <david.mendes@mindswap.academy>
2022-09-23 12:25:07 +0100

    event model

--- src/main/java/academy/mindswap/Mindera_Events/Model/Event.java
+++ src/main/java/academy/mindswap/Mindera_Events/Model/Event.java
@@ -1,4 +1,31 @@
 package academy.mindswap.Mindera_Events.Model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Document("events")
 public class Event {
+    @Id
+    private String id;
+    private String title;
+
+    private String type;
+
+    private String state;
+
+    private Date date;
+
+    private String startingTime;
+
+    public Event(String title, String type, Date date, String startingTime) {
+        this.title = title;
+        this.type = type;
+        this.state = "UNAPPROVED";
+        this.date = date;
+        this.startingTime = startingTime;
+    }
 }
