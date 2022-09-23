8681b602782d75108a9c99a5469e0f382739b118
fabioiketani <fabio.iketani@gmail.com>
2022-09-23 15:29:46 +0100

    get list services

--- src/main/java/academy/mindswap/Mindera_Events/Service/EventService.java
+++ src/main/java/academy/mindswap/Mindera_Events/Service/EventService.java
@@ -4,6 +4,8 @@ import academy.mindswap.Mindera_Events.Model.Event;
 import academy.mindswap.Mindera_Events.Repository.EventRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EventService {
     private final EventRepository eventRepository;
@@ -14,5 +16,9 @@ public class EventService {
     public Event createEvent(Event event) {
         //Event e = new Event();
         return eventRepository.insert(event);
+    }
+    public List<Event> getEventList() {
+        List<Event> eventList = eventRepository.findAll();
+        return eventList;
     }
 }
--- src/main/java/academy/mindswap/Mindera_Events/Service/UserService.java
+++ src/main/java/academy/mindswap/Mindera_Events/Service/UserService.java
@@ -1,7 +1,10 @@
 package academy.mindswap.Mindera_Events.Service;
 
+import academy.mindswap.Mindera_Events.Model.User;
 import academy.mindswap.Mindera_Events.Repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -10,4 +13,11 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+    public List<User> getUserList() {
+
+        List<User> userList = userRepository.findAll();
+
+        return userList;
+    }
+
 }
