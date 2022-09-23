59d10d31e7c10fc638596f613ad935009ea50af3
joaoguima24 <joao.guimaraes@mindswap.academy>
2022-09-23 12:01:58 +0100

    First commit

---
+++ src/main/java/academy/mindswap/Mindera_Events/MinderaEventsApplication.java
@@ -0,0 +1,13 @@
+package academy.mindswap.Mindera_Events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinderaEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinderaEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/mindswap/Mindera_Events/MinderaEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.mindswap.Mindera_Events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinderaEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
