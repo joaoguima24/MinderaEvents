6f29269a2712bf6bcb7022611de62eee5c8b050d
fabioiketani <fabio.iketani@gmail.com>
2022-09-23 15:10:48 +0100

    Users model

--- src/main/java/academy/mindswap/Mindera_Events/Controller/UserController.java
+++ src/main/java/academy/mindswap/Mindera_Events/Controller/UserController.java
@@ -1,7 +1,18 @@
 package academy.mindswap.Mindera_Events.Controller;
 
+import academy.mindswap.Mindera_Events.Service.EventService;
+import academy.mindswap.Mindera_Events.Service.UserService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/user")
 public class UserController {
+
+    private final UserService userService;
+    private final EventService eventService;
+
+    public UserController(UserService userService, EventService eventService) {
+        this.userService = userService;
+        this.eventService = eventService;
+    }
+
 }
