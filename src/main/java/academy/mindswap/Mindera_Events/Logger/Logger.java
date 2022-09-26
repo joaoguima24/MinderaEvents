d1c7a61e194036a3fa4a8686e4ab737eee068661
David Mendes <david.mendes@mindswap.academy>
2022-09-26 09:08:34 +0100

    Slf4j logger implementation

---
+++ src/main/java/academy/mindswap/Mindera_Events/Logger/Logger.java
@@ -0,0 +1,21 @@
+package academy.mindswap.Mindera_Events.Logger;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class Logger {
+
+    @Around("@annotation(LogExecutionTime)")
+    public  Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+}
--- src/main/java/academy/mindswap/Mindera_Events/MinderaEventsApplication.java
+++ src/main/java/academy/mindswap/Mindera_Events/MinderaEventsApplication.java
@@ -1,13 +1,21 @@
 package academy.mindswap.Mindera_Events;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@Slf4j
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class MinderaEventsApplication {
 
+	private final static Logger LOGGER = LoggerFactory.
+			getLogger(MinderaEventsApplication.class);
+
 	public static void main(String[] args) {
+		LOGGER.info("Starting App");
 		SpringApplication.run(MinderaEventsApplication.class, args);
 	}
 
