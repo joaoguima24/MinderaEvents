57287117877d20b63344fd6be9d9a90df94645e1
Carla Pereira <104978602+Interetion@users.noreply.github.com>
2022-09-27 22:58:45 +0100

    commmit waitinglist+

--- src/main/java/academy/mindswap/Mindera_Events/Service/EventService.java
+++ src/main/java/academy/mindswap/Mindera_Events/Service/EventService.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class EventService {
     private final EventRepository eventRepository;
     private final UserService userService;
+
     public EventService(EventRepository eventRepository, UserService userService) {
         this.eventRepository = eventRepository;
         this.userService = userService;
@@ -23,6 +24,7 @@ public class EventService {
 
     public EventDto createEvent(EventDto dto) {
         Event event = EventConverter.updateEventDto(dto);
+        //set to date and status
         event.setId("open");
         event.setDate("data actual");
         eventRepository.save(event);
@@ -36,16 +38,19 @@ public class EventService {
     }
 
     public ResponseEntity<List<DisplayEventListDto>> getByTitle(String title) throws EventNotFoundException {
-        if(eventRepository.findByTitle(title).stream().toList().isEmpty()){
-            throw new EventNotFoundException("No events found."); }
+        if (eventRepository.findByTitle(title).stream().toList().isEmpty()) {
+            throw new EventNotFoundException("No events found.");
+        }
         List<DisplayEventListDto> updateEvent = eventRepository.findByTitle(title).stream()
                 .map(EventConverter::getEventToDto)
                 .toList();
         return ResponseEntity.ok(updateEvent);
     }
+
     public ResponseEntity<List<DisplayEventListDto>> getByState(String state) throws EventNotFoundException {
-        if(eventRepository.findByState(state).stream().toList().isEmpty()){
-            throw new EventNotFoundException("No events found."); }
+        if (eventRepository.findByState(state).stream().toList().isEmpty()) {
+            throw new EventNotFoundException("No events found.");
+        }
         List<DisplayEventListDto> updateEvent = eventRepository.findByState(state).stream()
                 .map(EventConverter::getEventToDto)
                 .toList();
@@ -53,8 +58,9 @@ public class EventService {
     }
 
     public ResponseEntity<List<DisplayEventListDto>> getByDate(String date) throws EventNotFoundException {
-        if(eventRepository.findByDate(date).stream().toList().isEmpty()){
-            throw new EventNotFoundException("No events found."); }
+        if (eventRepository.findByDate(date).stream().toList().isEmpty()) {
+            throw new EventNotFoundException("No events found.");
+        }
         List<DisplayEventListDto> updateEvent = eventRepository.findByDate(date).stream()
                 .map(EventConverter::getEventToDto)
                 .toList();
@@ -62,8 +68,9 @@ public class EventService {
     }
 
     public ResponseEntity<List<DisplayEventListDto>> getByType(String type) throws EventNotFoundException {
-        if(eventRepository.findByType(type).stream().toList().isEmpty()){
-            throw new EventNotFoundException("No events found."); }
+        if (eventRepository.findByType(type).stream().toList().isEmpty()) {
+            throw new EventNotFoundException("No events found.");
+        }
         List<DisplayEventListDto> updateEvent = eventRepository.findByType(type).stream()
                 .map(EventConverter::getEventToDto)
                 .toList();
@@ -79,43 +86,46 @@ public class EventService {
     }
 
 
-    public void relateEventToUser(String userId, String id ) throws academy.mindswap.Mindera_Events.Exceptions.UserNotFoundException {
+    public void relateEventToUser(String userId, String id) throws academy.mindswap.Mindera_Events.Exceptions.UserNotFoundException {
 
 
-        Event event= eventRepository.findById(id).orElseThrow();
-        User user= UserConverter.DtoToUser(userService.getUserById(userId));
-        if (event.getAttendance() == null){
-            List<User> userList = new ArrayList<>();
-            event.setAttendance(userList);
-            eventRepository.save(event);
-        }
-        if (user.getEvents()== null){
-            List<String> idList = new ArrayList<>();
-            user.setEvents(idList);
-        }
-
-        if ((event.getAttendance().size() + 1)<= event.getSlots()){
-            event.getAttendance().add(user);
-            eventRepository.save(event);
+        Event event = eventRepository.findById(id).orElseThrow();
+        User user = UserConverter.DtoToUser(userService.getUserById(userId));
+        try {
 
             user.getEvents().parallelStream().filter(idEvent -> user.getEvents().equals(user.getEvents()));
 
-            user.getEvents().add(id);
-            userService.updateUser(userId,user);
 
-        } else if (event.getWaitingList()== null) {
-            List <User> listWaiting=new ArrayList<>();
-            event.setWaitingList(listWaiting);
-            // alterar id no model
-            event.getWaitingList().add(user);
-            eventRepository.save(event);
+        } catch (Exception exception) {
+            if (event.getAttendance() == null) {
+                List<User> userList = new ArrayList<>();
+                event.setAttendance(userList);
+                eventRepository.save(event);
+            }
+            if (user.getEvents() == null) {
+                List<String> idList = new ArrayList<>();
+                user.setEvents(idList);
+            }
+
+            if ((event.getAttendance().size() + 1) <= event.getSlots()) {
+                event.getAttendance().add(user);
+                eventRepository.save(event);
+
+
+                user.getEvents().add(id);
+                userService.updateUser(userId, user);
+
+            } else if (event.getWaitingList() == null) {
+                List<User> listWaiting = new ArrayList<>();
+                event.setWaitingList(listWaiting);
+                // alterar id no model
+                event.getWaitingList().add(user);
+                eventRepository.save(event);
+            }
+
+
         }
 
 
-
-        }
-
-
-
-
+    }
 }
