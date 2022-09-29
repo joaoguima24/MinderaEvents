83c650ca697773569d1883775eb6a1394d04bed7
joaoguima24 <joao.guimaraes@mindswap.academy>
2022-09-29 11:58:29 +0100

    Code review:

--- src/test/java/academy/mindswap/Mindera_Events/Controller/UserController.java
+++ src/test/java/academy/mindswap/Mindera_Events/Controller/UserController.java
@@ -1,5 +1,18 @@
 package academy.mindswap.Mindera_Events.Controller;
 
-public class UserController {
+import academy.mindswap.Mindera_Events.Repository.UserRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserController {
+    @MockBean
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
